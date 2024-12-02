920e3e37b2d890d6dc800ec4c6dc96a2807bcbd0
Victor Garrido <victorgarrido04@iesamachado.org>
2024-12-02 13:26:22 +0100

    02/12/2024

--- 1er trimestre/php2/diahoy.php
+++ 1er trimestre/php2/diahoy.php
@@ -1,4 +1,7 @@
 <?php
-strftime ($cadenaConMarcas, $valorTiempoEnSegundos)
-'Fecha actual: '.strftime("%A, %d de %B de %Y").'<br/>';
+    $dias = array("Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sábado");
+    $meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+     
+    echo $dias[date('w')]." ".date('d')." de ".$meses[date('n')-1]. " del ".date('Y') ;
+    //Resultado: Domingo 26 de Enero del 2020
 ?>
