bac4f41ae711944b084ad06e13010945a5c41548
Victor Garrido <victorgarrido04@iesamachado.org>
2024-12-02 13:43:41 +0100

    02/12/2024

--- 1er trimestre/php2/fechahora.php
+++ 1er trimestre/php2/fechahora.php
@@ -1,6 +1,7 @@
 <?php
     $dias = array("Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sábado");
     $meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+    $hora = time();
 
-    echo $dias[date('w')]." ".date('d')." de ".$meses[date('n')-1]. " del ".date('Y') ' H:i:s' ;
+    echo $dias[date('w')]." ".date('d')." de ".$meses[date('n')-1]. " del ".date('Y') . $hora-> format('H:i:s') ;
 ?>
