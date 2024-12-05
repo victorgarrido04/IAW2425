2fedad1f7058b7d677bddf42e6efef0890cb758b
victorgarrido04 <victorgarrido04@iesamachado.org>
2024-12-05 13:52:22 +0100

    05/11/2024

--- 1er trimestre/php2/fechahora.php
+++ 1er trimestre/php2/fechahora.php
@@ -1,4 +1,4 @@
-<?php
+i<?php
 $dias = array("Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado");
 $meses = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
 $fecha = $dias[date('w')] . " " . date('d') . " de " . $meses[date('n') - 1] . " del " . date('Y');
---
+++ 1er trimestre/php2/piramide.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Piramide Asteriscos</title>
+</head>
+<body>
+    <h1>Introduce un numero positivo</h1>
+    <input type="number" name="" id="">
+    <button type="submit">Enviar</button>
+</body>
+</html>
