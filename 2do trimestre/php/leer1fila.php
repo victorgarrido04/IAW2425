ee01606bb3b9389e5fd4d9c047d73c2818e1b779
Victor Garrido <victorgarrido04@iesamachado.org>
2025-01-15 12:31:50 +0100

    15/01/2025

---
+++ 2do trimestre/php/leer1fila.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "sql308.thsite.top";
+$username = "thsi_38097494";
+$password = "fDxz?Ica";
+$dbname = "thsi_38097494_bdpruebas";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, username, password FROM usuarios";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["username"]. " - Contra: " . $row["password"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?> 
